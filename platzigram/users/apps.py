ddeabdef49a045413604e3117421d114762d3400
dgsaltarin <dgsaltarin@gmail.com>
2021-01-31 16:49:30 -0500

    added requeriments.txt

---
+++ platzigram/users/apps.py
@@ -0,0 +1,9 @@
+""""User app configuration"""
+
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """"uSER APP CONFIG"""
+    name = 'users'
+    verbose_name = 'Users'
---
+++ platzigram/users/models.py
@@ -0,0 +1,5 @@
+""""User models"""
+from django.db import models
+
+
+# Create your models here.
